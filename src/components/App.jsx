65c895d0569d72ccf57f25ee6834e7692ad8eba8
codefromrvk <varshithkumar553@gmail.com>
2021-12-26 19:22:52 +0530

    'feat:create-add-note-option'

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,26 @@ import React, { useState } from "react";
 import Footer from "./Footer";
 import {Header} from "./Header"
 import Note from "./Note";
-import notes from "../note"
+// import notes from "../note"
 import InputArea from "./InputArea";
 
 
 
 export default function App(){
-  
+  const [notes,setNotes]= useState([])
+
+  function submitNote(note){
+        setNotes((prevValue)=>{
+            return [...prevValue,note]
+        })
+  }
+
+
+
     return (
     <>
     <Header/>
-    <InputArea/>
+    <InputArea onAdd={submitNote}/>
     {
      notes.map((note)=>  <Note key={note.key} title={note.title} content={note.content}/>)
     }
--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
-export default function InputArea(){
+export default function InputArea(props){
+
+    const [inputText,setInputText]=useState({
+        title:"",
+        content:""
+    })
+    
+    function addNote(e){
+        const {id,value}=e.target;
+        setInputText((prevValue)=>{
+            return {
+                ...prevValue,
+                [id]:value
+            }
+        })
+    }
+
+    
     return(
         <div className="input-area">
-            <input id="title-text" placeholder='Title' type="text"/>
-            <input id="input-text" placeholder='Write note' type="text"/>
-            <button className='add-button'><span>Add</span></button>
+            <input onChange={addNote} id="title" placeholder='Title' type="text"/>
+            <input onChange={addNote} id="content" placeholder='Write note' type="text"/>
+            <button onClick={()=>{props.onAdd(inputText)}} className='add-button'><span>Add</span></button>
         </div>
     )
 }
